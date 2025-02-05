ae7c9be9cf95da240ffb791b193d77af40ce3eda
Jarl <jarl13.work@gmail.com>
2025-02-06 03:45:23 +0800

    add: navbar & hero component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-const name = "Jarl";
+import Navbar from "@/components/Navbar.vue";
+import HeroVue from "@/components/Hero.vue";
 </script>
 
 <template>
-  <p class="text-4xl">
-    Hello i'm <span class="font-bold text-violet-500">{{ name }}</span>
-  </p>
+  <Navbar />
+  <HeroVue />
 </template>
